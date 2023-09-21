97f9719b2cb47f378a3cd04d4a776dda06cb86ab
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-09-21 14:43:04 -0300

    .

--- o-que-e-Typescript/assets/js/script.js
+++ o-que-e-Typescript/assets/js/script.js
@@ -1,12 +1,10 @@
-let numero1 = document.querySelector('#numero1');
-let numero2 = document.querySelector('#numero2');
-let botao = document.querySelector('#calcular');
-let res = document.querySelector('#resultado');
-
-function calcular(n1, n2){
-    return n1 + n2;    
+var numero1 = document.querySelector('#numero1');
+var numero2 = document.querySelector('#numero2');
+var botao = document.querySelector('#calcular');
+var res = document.querySelector('.resultado');
+function calcular(n1, n2) {
+    return n1 + n2;
 }
-
-botao.addEventListener('click', ()=>{
+botao.addEventListener('click', function () {
     res.innerHTML = calcular(numero1.value, numero2.value);
 });
