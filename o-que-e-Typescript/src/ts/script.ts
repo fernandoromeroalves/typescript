6d9ce9ae1adb00e241f876be41dada7b00b3bf89
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-09-22 03:06:56 -0300

    Os types primitivos

--- o-que-e-Typescript/src/ts/script.ts
+++ o-que-e-Typescript/src/ts/script.ts
@@ -3,10 +3,10 @@ let numero2 = document.querySelector('#numero2') as HTMLInputElement;
 let botao = document.querySelector('#calcular');
 let res = document.querySelector('.resultado');
 
-function calcular(n1, n2){
+function calcular(n1: number, n2: number) {
     return n1 + n2;    
 }
 
 botao.addEventListener('click', ()=>{
-    res.innerHTML = calcular(numero1.value, numero2.value);
+    res.innerHTML = calcular( +numero1.value, +numero2.value).toString();
 });
