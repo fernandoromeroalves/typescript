0ea587296515471bd5a9f7f6a43d49e2d71153ff
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-10-04 18:49:53 -0300

    .

---
+++ retorno-void/src/script.ts
@@ -0,0 +1,8 @@
+
+// tsc script.ts --noEmitOnError --outDir
+
+type QualquerFuncao = () => void;
+
+const algo: QualquerFuncao = () => {
+    return 'bla bla bla';
+}
--- types-para-funcoes/src/script.ts
+++ types-para-funcoes/src/script.ts
@@ -1,3 +1,20 @@
 
 // tsc script.ts --noEmitOnError --outDir
 
+type MathFunction = (n1: number, n2: number) => number;
+
+const somar: MathFunction = (n1 , n2) => {
+    return n1 + n2
+}
+
+const subtrair: MathFunction = (n1, n2) =>{
+    return n1 - n2
+};
+
+const multiplicar: MathFunction = (n1, n2) =>{
+    return n1 * n2
+};
+
+const divisor: MathFunction = (n1, n2) =>{
+    return n1 / n2
+}
